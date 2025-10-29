b463de99d991591beb530f7a074c60f53ce8899d
ashwanth-shyam-kumar <ashwanth.isro@gmail.com>
2025-10-29 19:04:20 +0530

    Add files via upload

---
+++ Graphics/sample1.tsx
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sample1" tilewidth="448" tileheight="448" tilecount="33" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="arean_fire.png" width="448" height="448"/>
+ </tile>
+ <tile id="1">
+  <image source="gate_pillar.png" width="64" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="arena_plant.png" width="448" height="448"/>
+ </tile>
+ <tile id="3">
+  <image source="arena_water.png" width="448" height="448"/>
+ </tile>
+ <tile id="4">
+  <image source="gate_top.png" width="232" height="80"/>
+ </tile>
+ <tile id="5">
+  <image source="grass.png" width="64" height="64"/>
+ </tile>
+ <tile id="6">
+  <image source="grass_ice.png" width="64" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="grassrock1.png" width="60" height="60"/>
+ </tile>
+ <tile id="8">
+  <image source="grassrock2.png" width="56" height="60"/>
+ </tile>
+ <tile id="9">
+  <image source="green_tree.png" width="128" height="192"/>
+ </tile>
+ <tile id="10">
+  <image source="green_tree_bushy.png" width="128" height="192"/>
+ </tile>
+ <tile id="11">
+  <image source="green_tree_small.png" width="64" height="128"/>
+ </tile>
+ <tile id="12">
+  <image source="hospital.png" width="384" height="384"/>
+ </tile>
+ <tile id="13">
+  <image source="house_large.png" width="448" height="448"/>
+ </tile>
+ <tile id="14">
+  <image source="house_large_alt.png" width="448" height="448"/>
+ </tile>
+ <tile id="15">
+  <image source="house_small.png" width="320" height="320"/>
+ </tile>
+ <tile id="16">
+  <image source="house_small_alt.png" width="320" height="320"/>
+ </tile>
+ <tile id="17">
+  <image source="ice_tree.png" width="128" height="192"/>
+ </tile>
+ <tile id="18">
+  <image source="icerock1.png" width="60" height="60"/>
+ </tile>
+ <tile id="19">
+  <image source="icerock2.png" width="56" height="60"/>
+ </tile>
+ <tile id="20">
+  <image source="palm.png" width="128" height="192"/>
+ </tile>
+ <tile id="21">
+  <image source="palm_alt.png" width="128" height="192"/>
+ </tile>
+ <tile id="22">
+  <image source="palm_small.png" width="64" height="128"/>
+ </tile>
+ <tile id="23">
+  <image source="ruin_gate.png" width="192" height="192"/>
+ </tile>
+ <tile id="24">
+  <image source="ruin_pillar.png" width="64" height="192"/>
+ </tile>
+ <tile id="25">
+  <image source="ruin_pillar_broke.png" width="64" height="104"/>
+ </tile>
+ <tile id="26">
+  <image source="ruin_pillar_broke_alt.png" width="64" height="128"/>
+ </tile>
+ <tile id="27">
+  <image source="sand.png" width="64" height="64"/>
+ </tile>
+ <tile id="28">
+  <image source="sandrock1.png" width="60" height="60"/>
+ </tile>
+ <tile id="29">
+  <image source="sandrock2.png" width="56" height="60"/>
+ </tile>
+ <tile id="30">
+  <image source="teal_tree.png" width="128" height="192"/>
+ </tile>
+ <tile id="31">
+  <image source="teal_tree_bushy.png" width="128" height="192"/>
+ </tile>
+ <tile id="32">
+  <image source="teal_tree_small.png" width="64" height="128"/>
+ </tile>
+</tileset>
